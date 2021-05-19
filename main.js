3b68c4c88250a83b78f62c27b1674274809a9f57
aniket8439 <aniket8439@gmail.com>
2021-05-19 15:56:03 +0530

    about

---
+++ main.js
@@ -0,0 +1,22 @@
+var navbar = document.getElementById("navbar");
+var menuHamburger= document.getElementById("ham");
+var sticky = navbar.offsetTop;
+
+// code for sticky navigation bar
+window.onscroll = () => {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+};
+
+// for hamburger menu
+
+menuHamburger.addEventListener('click',() => {
+  if(navbar.className === "main_navigation"){
+    navbar.classList.add("responsive");
+  } else {
+    navbar.classList.remove("responsive");
+  }
+});
